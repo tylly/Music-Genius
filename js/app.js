d700e87327d675d99430bc440f494d40e9df266d
jamestylladams <jamestylladams@gmail.com>
2022-06-22 13:04:27 -0600

    Add constant js file for global variables

--- js/app.js
+++ js/app.js
@@ -1,32 +1,3 @@
-//Here we declare gobal variables and DOM selectors for use in all js files.
-let topAlbums = []
-let topAlbumsGenre = []
-const findOut = document.getElementById("findOutArtist")
-const finOutGenre = document.getElementById("findOutGenre")
-const startOver = document.getElementById("startOver")
-let head = document.getElementById("head")
-let input = document.getElementById("artist")
-let inputGenre = document.getElementById("genre")
-let url
-let questionContainer = document.getElementById("questionContainer")
-let prompt = document.getElementById("prompt")
-let prompt2 = document.getElementById("prompt2")
-const or = document.getElementById("or")
-let question = document.getElementById("question")
-let genreImage = document.getElementById("genreImage")
-let boxOneImg = document.getElementById("boxOneImg")
-let boxTwoImg = document.getElementById("boxTwoImg")
-let boxThreeImg = document.getElementById("boxThreeImg")
-let boxFourImg = document.getElementById("boxFourImg")
-let boxBut = document.getElementsByClassName("boxBut")
-let butA = document.getElementById("boxButA")
-let butB = document.getElementById("boxButB")
-let butC = document.getElementById("boxButC")
-let butD = document.getElementById("boxButD")
-let scoreDisplay = document.getElementById("score")
-let turn = 1
-let actualScore = 0
-
 //These are two valid API keys for the LastFM API. I requested two in case I accidentally exceed their usuage limits outlined in their documentation
 //b6d97def09e924303dab1c829302163b
 //0475e4b7b017bd6c657020d0458d38ac
